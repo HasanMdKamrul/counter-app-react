0cb35f3ef55cf22f0d43c3b103c5523d90ed04fc
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-20 02:37:18 +0200

    Counter with tailwind css done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App text-white">
-     <h1 className='text-5xl my-5'>Counter</h1>
+     <h1 className='text-5xl my-5 font-semibold'>COUNTER</h1>
      <Counter></Counter>
     </div>
   );
@@ -19,9 +19,9 @@ function Counter(){
 
   return(
     <div className='my-5'>
-      <h1 className='text-3xl'>Count:{count < 0 ? 'Nagetive value no possible' : count}</h1>
-      <button onClick={increaseHandle} type="button" className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 my-5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800">Increase</button>
-      <button onClick={decreaseHandle} type="button" className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800">Decrease</button>
+      <h1 className='text-3xl font-extrabold'>Count:{count < 0 ? 'Nagetive value no possible' : count}</h1>
+      <button onClick={increaseHandle} type="button" className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 my-5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800"><i className="fa-solid fa-plus"></i></button>
+      <button onClick={decreaseHandle} type="button" className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800"><i className="fa-solid fa-minus"></i></button>
       <button onClick={resetHandle} type="button" className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800">Reset</button>
     </div>
   )
